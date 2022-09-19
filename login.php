216a27d0e0400740b1298897aa702039e2c08e54
humphreyokoth <humpasp@gmail.com>
2022-09-19 10:24:40 +0300

    Feat:Ajax Json

--- login.php
+++ login.php
@@ -1,6 +1,8 @@
 <?php 
 if(isset($_POST['username']) && $_POST['username'] && isset($_POST['password']) && $_POST['password']){
-    
+    echo json_encode(array('success'=>1));
+}else {
+    echo json_encode(array('success'=>0));
 }
 
 ?>
