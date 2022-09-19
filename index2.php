b099f2cc38394a609c82c9f1273aee266d97a604
humphreyokoth <humpasp@gmail.com>
2022-09-19 09:53:53 +0300

    Feat:Ajax Jquery script

--- index2.php
+++ index2.php
@@ -9,7 +9,36 @@
 </head>
 <body>
   <form id="loginform" method="post">
-    
-  </form>  
+    <div>
+        Username:
+        <input type="text" name="username" id="username"/>
+        Password:
+        <input type="password" name="password" id="password"/>
+        <input type="submit" name="loginBtn" id="loginBtn" value="Login">
+    </div>
+  </form>
+  <script type="text/javascript">
+    $(document).ready(function(){
+    $("#loginform").submit(function(e){
+        e.preventDefault();
+        $.ajax({
+            type:"POST",
+            url:"login.php",
+            data:$(this).serialize(),
+            success:function(response){
+                var jsonData = JSON.parse(response);
+                // user is logged in successfully in the backend
+                //Let's redirect
+                if(jsonData.success == "1"){
+                    location.href = "my_profile.php";
+                }else{
+                    alert("Invalid Credentials");
+                }
+            }
+        });
+    });
+    });
+
+  </script>
 </body>
 </html>
