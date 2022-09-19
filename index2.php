b3c34fc4d070635ab017397e8f0dd2106c3e5348
humphreyokoth <humpasp@gmail.com>
2022-09-19 11:04:29 +0300

    Feat:Ajax Jquery script

--- index2.php
+++ index2.php
@@ -8,6 +8,8 @@
     <script src=”https://code.jquery.com/jquery-3.3.1.js” integrity=”sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=” crossorigin=”anonymous”></script>
 </head>
 <body>
+    <?php 
+    include './login.php'?>
   <form id="loginform" method="post">
     <div>
         Username:
